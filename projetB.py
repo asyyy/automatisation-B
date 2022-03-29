7da4f72e947f9dcc16a8022040b2b2d3ba5e2268
Alexys Guerin <alexys.guerin@dolmen-tech.com>
2022-03-29 09:23:19 +0200

    wip

--- projetB.py
+++ projetB.py
@@ -21,8 +21,19 @@ boite_RH = []
 boite_LF = []
 
 
-# FUNCTIONS (TODO les commenter parce que la c'est chaud)
+random_PAB_BS = []
+random_PAB_RH = []
+random_PAB_LF = []
+random_PAB_RO = []
 
+random_PCD_BS = []
+random_PCD_RH = []
+random_PCD_LF = []
+random_PCD_RO = []
+
+# Find the longest word in a list
+# Param => list : List
+# Return => int : nomber of char in the longest word
 def find_longest_word(list):
     max_length = 0
     for x in list:
@@ -30,12 +41,24 @@ def find_longest_word(list):
             max_length = len(x)
     return max_length
 
+# Write in the excel a list from top to bot at the column index given by the user
+# Param => col : index of the column to write the list
+# Param => title : title to write in the first row
+# Param => list : list to write
 def write_list_in_excel(col,title,list):
     worksheet.set_column(col, col, find_longest_word(list))
     worksheet.write(0,col,title,title_cell)
     for row in range(len(list)):
         worksheet.write(row+1, col, list[row])
 
+# Write in the excel a box from a list with the given size 
+# Param => length : length of the box
+# Param => width : width of the box
+# Param => global_col : index of the col to start to write
+# Param => global_row : index of the row to start to write
+# Param => list : list to write
+# Param => index : index of the element in the list to start
+# Param => box_number : number of the box 
 def box(length,width,global_col,global_row,list,index,box_number):
     worksheet.set_column(global_col, global_col+width-1, find_longest_word(list))
     worksheet.merge_range(global_row-1,global_col,global_row-1, global_col+width-1, 'Merged Range')
@@ -57,7 +80,24 @@ def boxs(length,width,global_col,list, title):
         index = box(length,width,global_col,global_row+box_number,list,index,str(box_number)+ " " +title)
         global_row = global_row + length
         box_number = box_number + 1
+        
+# Will use ranom_box function until there are enought element in the list
+# Param => length : length of the box
+# Param => width : width of the box
+# Param => global_col : index of the col to start to write
+# Param => global_row : index of the row to start to write
+# Param => list : list to write
+def random_boxes(title, length,width,global_col,list):
+    index = 0
+    box_number = 0
+    global_row = 1
 
+    while index < len(list):
+        index = random_box(title, length,width,global_col, global_row+box_number,list,index,box_number)
+        global_row = global_row+length
+        box_number = box_number + 1
+        
+# Return a random element from the list and remove this element from the list
 def random_coor(list):
     if list:
         r = random.randint(0,len(list)-1)
@@ -72,9 +112,6 @@ def random_box(title, length, width, global_col, list):
     for i in range(length):
         for j in range(width):
             random_box_coordinate.append([i,j])
-    # print(random_box_coordinate)
-    # print("len random: " + str(len(random_box_coordinate)))
-    # print("len list: " + str(len(list)))
     
     if len(list) < len(random_box_coordinate):
         max = len(list)
@@ -90,18 +127,53 @@ def random_box(title, length, width, global_col, list):
     dead_columns = ["Truc : "]
     for row in range(length):
         worksheet.write(row+1,width+global_col, dead_columns[0]+str(row),yellow_cell)
+
+
+# Old method of random_box
+# def random_box(title, length, width, global_col, list):
+#     worksheet.set_column(global_col, global_col+width, find_longest_word(list))
+#     worksheet.merge_range(0,global_col,0, global_col+width, 'Merged Range')
+#     worksheet.write(0,global_col,"Box Random "+title, title_cell)
+#     random_box_coordinate = []
+
+#     for i in range(length):
+#         for j in range(width):
+#             random_box_coordinate.append([i,j])
+#     # print(random_box_coordinate)
+#     # print("len random: " + str(len(random_box_coordinate)))
+#     # print("len list: " + str(len(list)))
+    
+#     if len(list) < len(random_box_coordinate):
+#         longest_list = len(list)
+#     else: 
+#         longest_list = len(random_box_coordinate)
+
+#     for x in range(longest_list):
+#         coor = random_coor(random_box_coordinate)
+#         worksheet.write(coor[0]+1, coor[1]+global_col, list[x])
+
+#     yellow_cell = workbook.add_format()
+#     yellow_cell.set_bg_color('yellow')
+#     dead_columns = ["Truc : "]
+#     for row in range(length):
+#         worksheet.write(row+1,width+global_col, dead_columns[0]+str(row),yellow_cell)
+        
 # Prefixe generator  = AfXXX
-def prefix_count(prefix,index):
-    if index < 10 :
-        return prefix + "00" + str(index)
-    if 10 <= index < 100:
-        return prefix + "0"  + str(index)
-    if 100 <= index < 1000:
-        return prefix + str(index)
-    if index <= 1000:
+# Param => prefix : string, the prefix to write
+# Param => number : int, the number to adapte
+# Return => string : the correcte association between the prefix and the number
+# Af001 / Af010
+def prefix_count(prefix,number):
+    if number < 10 :
+        return prefix + "00" + str(number)
+    if 10 <= number < 100:
+        return prefix + "0"  + str(number)
+    if 100 <= number < 1000:
+        return prefix + str(number)
+    if number <= 1000:
         return "Count > 1000 is not implement."
 
-# List generator (TODO A mettre dans une fonction)
+# Create the main list to be used in the program
 def list_generator():
     for x in range(countMin,countMax+1):
     
@@ -152,15 +224,18 @@ def non_negative_input(output):
         try:
             value = int(input(output))
         except ValueError:
-            print("Sorry, I didn't understand that.")
+            print("Ce n'est pas un nobmre.")
             continue
 
         if value < 0 or value >= 1000:
-            print("Sorry, your response must not be negative and under 1000")
+            print("Votre réponse doit être entre 0 et 1000.")
+        elif not value: 
+            print("Votre réponse ne doit pas être vide.")
         else:
             break
     return value
 
+# Generate an input for user and check if it's a word and not empty
 def non_empty_string_input(output):
     while True:
         try:
@@ -177,32 +252,42 @@ def non_empty_string_input(output):
             break
     return value
 
+# Main
+workbook = xlsxwriter.Workbook('projetB.xlsx')
+worksheet = workbook.add_worksheet()
+
+
+title_cell = workbook.add_format()
+title_cell.set_bg_color('green')
+title_cell.set_font_size(18)
+title_cell.set_align('center')
+
 
 print("Pour quitter l'opération en cours, faite CTRL+C.")
-# prefix = non_empty_string_input("Veuillez entrer le prefix : ")
-# countMin = non_negative_input("Veuillez entrer la borne min (>0) : ") 
-# countMax = non_negative_input("Veuillez entrer la borne max (>0) : ")
-# name = non_empty_string_input("Veuillez entrer le nom de votre truc (ex: Bn): ")
-# year = non_empty_string_input("Veuillez entrer l'année (ex: Y21) : ")
-# season = non_empty_string_input("Veuillez entrer la saison (ex: Au) : ")
-# length_box= non_negative_input("Veuillez entrer la longueur de votre boite normal : ")
-# width_box= non_negative_input("Veuillez entrer la largeur de votre boite normal : ")
-# length_randombox= non_negative_input("Veuillez entrer la longueur de votre boite aléatoire : ")
-# width_randombox= non_negative_input("Veuillez entrer la largeur de votre boite aléatoire : ")
+prefix = non_empty_string_input("Veuillez entrer le prefix : ")
+countMin = non_negative_input("Veuillez entrer la borne min (>0) : ") 
+countMax = non_negative_input("Veuillez entrer la borne max (>0) : ")
+name = non_empty_string_input("Veuillez entrer le nom de votre truc (ex: Bn): ")
+year = non_empty_string_input("Veuillez entrer l'année (ex: Y21) : ")
+season = non_empty_string_input("Veuillez entrer la saison (ex: Au) : ")
+length_box= non_negative_input("Veuillez entrer la longueur de votre boite normal : ")
+width_box= non_negative_input("Veuillez entrer la largeur de votre boite normal : ")
+length_randombox= non_negative_input("Veuillez entrer la longueur de votre boite aléatoire : ")
+width_randombox= non_negative_input("Veuillez entrer la largeur de votre boite aléatoire : ")
 
-prefix = "TestM"
-countMin = 0
-countMax = 25
-name = "Bn"
-year = "Y22"
-season = "Sp"
-length_box= 10
-width_box= 10
-length_randombox= 8
-width_randombox= 12
+# prefix = "TestM"
+# countMin = 0
+# countMax = 25
+# name = "Bn"
+# year = "Y22"
+# season = "Sp"
+# length_box= 10
+# width_box= 10
+# length_randombox= 8
+# width_randombox= 12
 
 tab1 = ["PA","PB","PC","PD"]
-tab1bis = ["PA","PB","PC","PD","Culturomique"]
+tab1bis = ["PA","PB","PC","PD","Culturomique","Culturomique"]
 tab2 = ["BS","RH","LF","RO"]
 
 
@@ -233,11 +318,11 @@ boxs(length_box,width_box, 13+width_box*4, boite_RH, "RH")
 
 
 # Random Box PA PB
-random_box("PA_PB",length_randombox,width_randombox, 15 + width_box*5, tubes_PA_PB)
+random_boxes("PA_PB",length_randombox,width_randombox, 15 + width_box*5, tubes_PA_PB)
 
 # Random Box PC PD
 
-random_box("PC_PD",length_randombox,width_randombox, 17 + width_box*6+width_randombox, tubes_PC_PD)
+random_boxes("PC_PD",length_randombox,width_randombox, 17 + width_box*6+width_randombox, tubes_PC_PD)
 
 #Random Box
 
